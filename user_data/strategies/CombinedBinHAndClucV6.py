973cb38a6ad7bd2a5c8fbcfbf2600057ced8dfde
Jason Too <tooyipjee@gmail.com>
2021-05-20 21:54:28 +0100

    Updated configs for USDT

--- user_data/strategies/CombinedBinHAndClucV6.py
+++ user_data/strategies/CombinedBinHAndClucV6.py
@@ -1,19 +1,17 @@
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 import numpy as np
 import talib.abstract as ta
-from freqtrade.strategy import merge_informative_pair
 from freqtrade.strategy.interface import IStrategy
-from freqtrade.persistence import Trade
 from pandas import DataFrame
 from datetime import datetime, timedelta
 
 
 ###########################################################################################################
-##                CombinedBinHAndClucV6 by iterativ                                                      ##
+##                CombinedBinHAndClucV5 by iterativ                                                      ##
 ##                                                                                                       ##
-##    Freqtrade https://github.com/freqtrade/freqtrade                                                    ##
+##    Fretrade https://github.com/freqtrade/freqtrade                                                    ##
 ##    The authors of the original CombinedBinHAndCluc https://github.com/freqtrade/freqtrade-strategies  ##
-##    V6 by iterativ.                                                                                    ##
+##    V5 by iterativ.                                                                                    ##
 ##                                                                                                       ##
 ###########################################################################################################
 ##               GENERAL RECOMMENDATIONS                                                                 ##
@@ -21,7 +19,6 @@ from datetime import datetime, timedelta
 ##   For optimal performance, suggested to use between 4 and 6 open trades, with unlimited stake.        ##
 ##   A pairlist with 20 to 40 pairs. Volume pairlist works well.                                         ##
 ##   Prefer stable coin (USDT, BUSDT etc) pairs, instead of BTC or ETH pairs.                            ##
-##   Highly recommended to blacklist leveraged tokens (*BULL, *BEAR, *UP, *DOWN etc).                    ##
 ##   Ensure that you don't override any variables in you config.json. Especially                         ##
 ##   the timeframe (must be 5m) & sell_profit_only (must be true).                                       ##
 ##                                                                                                       ##
@@ -35,30 +32,17 @@ from datetime import datetime, timedelta
 ##                                                                                                       ##
 ###########################################################################################################
 
-# SSL Channels
-def SSLChannels(dataframe, length = 7):
-    df = dataframe.copy()
-    df['ATR'] = ta.ATR(df, timeperiod=14)
-    df['smaHigh'] = df['high'].rolling(length).mean() + df['ATR']
-    df['smaLow'] = df['low'].rolling(length).mean() - df['ATR']
-    df['hlv'] = np.where(df['close'] > df['smaHigh'], 1, np.where(df['close'] < df['smaLow'], -1, np.NAN))
-    df['hlv'] = df['hlv'].ffill()
-    df['sslDown'] = np.where(df['hlv'] < 0, df['smaHigh'], df['smaLow'])
-    df['sslUp'] = np.where(df['hlv'] < 0, df['smaLow'], df['smaHigh'])
-    return df['sslDown'], df['sslUp']
-
 
 class CombinedBinHAndClucV6(IStrategy):
     INTERFACE_VERSION = 2
 
     minimal_roi = {
-        "0": 0.0181
+        "0": 0.018
     }
 
-    stoploss = -0.99 # effectively disabled.
+    stoploss = -0.25# effectively disabled.
 
     timeframe = '5m'
-    inf_1h = '1h' # informative tf
 
     # Sell signal
     use_sell_signal = True
@@ -79,7 +63,7 @@ class CombinedBinHAndClucV6(IStrategy):
     process_only_new_candles = False
 
     # Number of candles the strategy requires before producing valid signals
-    startup_candle_count: int = 200
+    startup_candle_count: int = 50
 
     # Optional order type mapping.
     order_types = {
@@ -96,39 +80,8 @@ class CombinedBinHAndClucV6(IStrategy):
             return 0.01
         return 0.99
 
-    def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
-                           rate: float, time_in_force: str, sell_reason: str, **kwargs) -> bool:
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        last_candle = dataframe.iloc[-1].squeeze()
-        # Prevent ROI trigger, if there is more potential, in order to maximize profit
-        if (last_candle['rsi'] > 50):
-            return False
-
-        return True
-
-    def informative_pairs(self):
-        pairs = self.dp.current_whitelist()
-        informative_pairs = [(pair, '1h') for pair in pairs]
-        return informative_pairs
-
-    def informative_1h_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        assert self.dp, "DataProvider is required for multiple timeframes."
-        # Get the informative pair
-        informative_1h = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe=self.inf_1h)
-        # EMA
-        informative_1h['ema_50'] = ta.EMA(informative_1h, timeperiod=50)
-        informative_1h['ema_200'] = ta.EMA(informative_1h, timeperiod=200)
-        # RSI
-        informative_1h['rsi'] = ta.RSI(informative_1h, timeperiod=14)
-        # SSL Channels
-        ssl_down_1h, ssl_up_1h = SSLChannels(informative_1h, 20)
-        informative_1h['ssl_down'] = ssl_down_1h
-        informative_1h['ssl_up'] = ssl_up_1h
-
-        return informative_1h
-
-    def normal_tf_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-         # strategy BinHV45
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # strategy BinHV45
         bb_40 = qtpylib.bollinger_bands(dataframe['close'], window=40, stds=2)
         dataframe['lower'] = bb_40['lower']
         dataframe['mid'] = bb_40['mid']
@@ -144,62 +97,25 @@ class CombinedBinHAndClucV6(IStrategy):
         dataframe['ema_slow'] = ta.EMA(dataframe, timeperiod=50)
         dataframe['volume_mean_slow'] = dataframe['volume'].rolling(window=30).mean()
 
-        # EMA
-        dataframe['ema_50'] = ta.EMA(dataframe, timeperiod=50)
-        dataframe['ema_200'] = ta.EMA(dataframe, timeperiod=200)
-
-        # SMA
-        dataframe['sma_5'] = ta.EMA(dataframe, timeperiod=5)
-
-        # RSI
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
-
-        return dataframe
-
-    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # The indicators for the 1h informative timeframe
-        informative_1h = self.informative_1h_indicators(dataframe, metadata)
-        dataframe = merge_informative_pair(dataframe, informative_1h, self.timeframe, self.inf_1h, ffill=True)
-
-        # The indicators for the normal (5m) timeframe
-        dataframe = self.normal_tf_indicators(dataframe, metadata)
-
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (  # strategy BinHV45
-                (dataframe['close'] > dataframe['ema_200_1h']) &
-                (dataframe['ema_50'] > dataframe['ema_200']) &
-                (dataframe['ema_50_1h'] > dataframe['ema_200_1h']) &
-
                 dataframe['lower'].shift().gt(0) &
-                dataframe['bbdelta'].gt(dataframe['close'] * 0.031) &
-                dataframe['closedelta'].gt(dataframe['close'] * 0.018) &
-                dataframe['tail'].lt(dataframe['bbdelta'] * 0.233) &
+                dataframe['bbdelta'].gt(dataframe['close'] * 0.008) &
+                dataframe['closedelta'].gt(dataframe['close'] * 0.0175) &
+                dataframe['tail'].lt(dataframe['bbdelta'] * 0.25) &
                 dataframe['close'].lt(dataframe['lower'].shift()) &
                 dataframe['close'].le(dataframe['close'].shift()) &
                 (dataframe['volume'] > 0) # Make sure Volume is not 0
             )
             |
             (  # strategy ClucMay72018
-                (dataframe['close'] > dataframe['ema_200']) &
-                (dataframe['close'] > dataframe['ema_200_1h']) &
-
                 (dataframe['close'] < dataframe['ema_slow']) &
-                (dataframe['close'] < 0.993 * dataframe['bb_lowerband']) &
-                (dataframe['volume'] < (dataframe['volume_mean_slow'].shift(1) * 21)) &
-                (dataframe['volume'] > 0)
-            )
-            |
-            (
-                (dataframe['close'] < dataframe['sma_5']) &
-                (dataframe['ssl_up_1h'] > dataframe['ssl_down_1h']) &
-                (dataframe['ema_50'] > dataframe['ema_200']) &
-                (dataframe['ema_50_1h'] > dataframe['ema_200_1h']) &
-
-                (dataframe['rsi'] < dataframe['rsi_1h'] - 43.276) &
-                (dataframe['volume'] > 0)
+                (dataframe['close'] < 0.985 * dataframe['bb_lowerband']) &
+                (dataframe['volume'] < (dataframe['volume_mean_slow'].shift(1) * 20)) &
+                (dataframe['volume'] > 0) # Make sure Volume is not 0
             ),
             'buy'
         ] = 1
